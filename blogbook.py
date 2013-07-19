2959924066fa9d40e2e820b494a3b27d22d923da
Daniel Greenfeld <pydanny@gmail.com>
2013-07-19 15:11:12 +0200

    keep introduction

--- blogbook.py
+++ blogbook.py
@@ -8,11 +8,35 @@ INDEX = """
 Inside the Head of Pydanny: E-book Edition
 ========================================
 
+Inside the Head of Pydanny: E-book Edition
+1st Edition
+by Daniel Greenfeld
+
+Copyright  2013 Daniel Greenfeld and Cartwheel Web.
+
+All rights reserved. This book may not be reproduced in any form, in whole or 
+in part, without written permission from the authors, except in the case of
+brief quotations embodied in articles or reviews.
+
+Limit of Liability and Disclaimer of Warranty: The author has used his best
+efforts in preparing this book, and the information provided herein ``as is.''
+The information provided is sold without warranty, either express or implied.
+Neither the author nor Cartwheel Web will be held liable for any damages to be
+caused either directly or indirectly by the contents of this book.
+
+Trademarks: Rather than indicating every occurrence of a trademarked name as
+such, this book uses the names only in an editorial fashion and to the benefit
+of the trademark owner with no intention of infringement of the trademark.
+
+First printing, July 2013
+
+
 Table of Contents:
 
 .. toctree::
    :maxdepth: 2
    
+   introduction
 {{ index }}
 """
 
